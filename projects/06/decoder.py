1e8f187876e1bc380aa143d5db3ee17af6bccf1d
bastionsofwill <bastionsofwill@gmail.com>
2022-07-24 17:46:03 +0900

    prj06 start

---
+++ projects/06/decoder.py
@@ -0,0 +1,17 @@
+def dest(mnmn):
+    regM = '0'
+    regA = '0'
+    regD = '0'
+    return regM + regA +regD 
+
+def comp(mnmn):
+    regM = '0'
+    regA = '0'
+    regD = '0'
+    return regM + regA +regD 
+
+def jump(mnmn):
+    lt = '0'
+    eq = '0'
+    gt = '0'
+    return lt + eq + gt 
---
+++ projects/06/parser.py
@@ -0,0 +1,25 @@
+def has_more_commands():
+    is_there_more = False 
+    return is_there_more
+
+def advance():
+    return
+
+def commandType():
+    cmd_list = ['A_COMMAND', 'C_COMMAND', 'L_COMMAND']
+    return cmd_list[0]
+
+def symbol():
+    return
+
+def symbol():
+    return
+
+def dest():
+    return
+
+def comp():
+    return
+
+def jump():
+    return
