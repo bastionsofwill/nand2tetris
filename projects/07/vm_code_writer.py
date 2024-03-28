3806c7d39e29460adce43c5f392c222513ee8165
bastionsofwill <bastionsofwill@gmail.com>
2024-03-28 17:43:34 +0900

    todo: eq,gt,lt vm commands

--- projects/07/vm_code_writer.py
+++ projects/07/vm_code_writer.py
@@ -8,67 +8,114 @@ class CodeWriter:
         return
 
     def write_arithmetic(self, command):
-        # match command:
-        #     case "add":
-
-        #     case "sub":
-        #     case "neg":
-        #     case "eq":
-        #     case "gt":
-        #     case "lt":
-        #     case "and":
-        #     case "or":
-        #     case "not":
-        #     case _:
-        #         print("unexpected arithmetic command")
+        # pop R13
+        self.output.write(f"@SP\n")
+        self.output.write(f"M=M-1\n")
+        self.output.write(f"A=M\n")
+        self.output.write(f"D=M\n")
+        self.output.write(f"@R13\n")
+        self.output.write(f"M=D\n")
+        if command not in ["neg", "not"]:
+            # pop R14
+            self.output.write(f"@SP\n")
+            self.output.write(f"M=M-1\n")
+            self.output.write(f"A=M\n")
+            self.output.write(f"D=M\n")
+            self.output.write(f"@R14\n")
+            self.output.write(f"M=D\n")
+        match command:
+            case "add":
+                # self.output.write(f"@R13\n")
+                # self.output.write(f"D=M\n")
+                # self.output.write(f"@R14\n")
+                # self.output.write(f"D=D+M\n")
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=D+M\n")
+            case "sub":
+                # self.output.write(f"@R13\n")
+                # self.output.write(f"D=M\n")
+                # self.output.write(f"@R14\n")
+                # self.output.write(f"D=D-M\n")
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=M-D\n")
+            case "neg":
+                self.output.write(f"D=-D\n")
+            case "eq":
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=M-D")
+                self.output.write(f"@R15\n")
+                self.output.write(f"D;JEQ")
+            case "gt":
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=M-D")
+                self.output.write(f"@R15\n")
+                self.output.write(f"D;JEQ")
+            case "lt":
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=M-D")
+                self.output.write(f"@R15\n")
+                self.output.write(f"D;JEQ")
+            case "and":
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=D&M\n")
+            case "or":
+                self.output.write(f"@R13\n")
+                self.output.write(f"D=D|M\n")
+            case "not":
+                self.output.write(f"D=!D\n")
+            case _:
+                print("unexpected arithmetic command")
+                return
+        # push D
+        self.output.write(f"@SP\n")
+        self.output.write(f"A=M\n")
+        self.output.write(f"M=D\n")
+        self.output.write(f"@SP\n")
+        self.output.write(f"M=M+1\n")
         self.output.write("\n")
         return
 
     def write_push_pop(self, command, segment, index):
         match segment:
             case "constant":
-                segment_symbol = str(index)
+                segment_ref = f"@{index}\n"
             case "local": 
-                segment_symbol = "LCL"
+                segment_ref = f"@LCL\n"
             case "argument":
-                segment_symbol = "ARG"
+                segment_ref = f"@ARG\n"
             case "this":
-                segment_symbol = "THIS"
+                segment_ref = f"@{index}\nD=A\n@THIS\nA=D+M\n"
             case "that":
-                segment_symbol = "THAT"
+                segment_ref = f"@{index}\nD=A\n@THAT\nA=D+M\n"
             case "pointer":
-                if index == '0':
-                    segment_symbol = "THIS"
-                elif index == '1':
-                    segment_symbol = "THAT"
+                if index == 0:
+                    segment_ref = f"@THIS\n"
+                elif index == 1:
+                    segment_ref = f"@THAT\n"
             case "temp":
-                segment_symbol = f"R{5 + index}"
+                segment_ref = f"@{5 +int(index)}\n"
             case "static":
-                segment_symbol = f"{self.file_name}.{self.static_count}"
+                segment_ref = f"@{self.file_name}.{self.static_count}\n"
                 self.static_count += 1
         match command:
-            case "C_PUSH":
-                self.output.write(f"@{segment_symbol}")
-                # if segment_symbol in ["LCL, ARG, THIS, THAT"]:
-                #     for i in range(index):
-                #         self.output.write("A=A+1")
-                # self.output.write(f"D=A")
-                # self.output.write(f"@SP")
-                # self.output.write(f"A=M")
-                # self.output.write(f"M=D")
-                # self.output.write(f"@SP")
-                # self.output.write(f"M=M+1")
-            case "C_POP":
-                self.output.write(f"")
-                # self.output.write(f"")
-                # self.output.write(f"")
-                # self.output.write(f"")
-                # self.output.write(f"@${segment_symbol}")
-                # self.output.write(f"M=D")
-                # self.output.write(f"@SP")
-                # self.output.write(f"M=M-1")
+            case "C_PUSH": # R[SP++] = x
+                self.output.write(segment_ref)
+                self.output.write(f"D=M\n")
+                self.output.write(f"@SP\n")
+                self.output.write(f"A=M\n")
+                self.output.write(f"M=D\n")
+                self.output.write(f"@SP\n")
+                self.output.write(f"M=M+1\n")
+            case "C_POP": # x = R[--SP]
+                self.output.write(f"@SP\n")
+                self.output.write(f"M=M-1\n")
+                self.output.write(f"A=M\n")
+                self.output.write(f"D=M\n")
+                self.output.write(segment_ref)
+                self.output.write(f"M=D\n")
             case _:
-                print("not a push/pop command")
+                print("unexpected push/pop command")
+                return
         return
 
     def close(self):
